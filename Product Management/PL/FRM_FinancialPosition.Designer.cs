8815d6a04ca1a9af309159d2261eb04896cb2b07
Ahmed Khojaly <Ahmed Khojaly@DESKTOP-A833B64>
2023-10-01 02:37:10 +0200

    Update Reporting

--- Product Management/PL/FRM_FinancialPosition.Designer.cs
+++ Product Management/PL/FRM_FinancialPosition.Designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_FinancialPosition));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.pieChart1 = new LiveCharts.WinForms.PieChart();
             this.txtPur = new System.Windows.Forms.TextBox();
             this.txtSeal = new System.Windows.Forms.TextBox();
@@ -38,16 +41,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.pieChart2 = new LiveCharts.WinForms.PieChart();
             this.txtCust = new System.Windows.Forms.TextBox();
             this.txtSup = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -66,23 +66,50 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(685, 335);
+            this.groupBox1.Size = new System.Drawing.Size(685, 390);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "حجم الاموال ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(143, 348);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "ج.س";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(142, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "ج.س";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(143, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ج.س";
             // 
             // pieChart1
             // 
             this.pieChart1.Location = new System.Drawing.Point(6, 22);
             this.pieChart1.Name = "pieChart1";
-            this.pieChart1.Size = new System.Drawing.Size(673, 212);
+            this.pieChart1.Size = new System.Drawing.Size(673, 247);
             this.pieChart1.TabIndex = 12;
             this.pieChart1.Text = "pieChart1";
             // 
             // txtPur
             // 
             this.txtPur.Enabled = false;
-            this.txtPur.Location = new System.Drawing.Point(206, 273);
+            this.txtPur.Location = new System.Drawing.Point(184, 310);
             this.txtPur.Name = "txtPur";
             this.txtPur.Size = new System.Drawing.Size(264, 23);
             this.txtPur.TabIndex = 11;
@@ -90,7 +117,7 @@
             // txtSeal
             // 
             this.txtSeal.Enabled = false;
-            this.txtSeal.Location = new System.Drawing.Point(206, 308);
+            this.txtSeal.Location = new System.Drawing.Point(184, 345);
             this.txtSeal.Name = "txtSeal";
             this.txtSeal.Size = new System.Drawing.Size(264, 23);
             this.txtSeal.TabIndex = 10;
@@ -98,7 +125,7 @@
             // txtExp
             // 
             this.txtExp.Enabled = false;
-            this.txtExp.Location = new System.Drawing.Point(206, 240);
+            this.txtExp.Location = new System.Drawing.Point(184, 277);
             this.txtExp.Name = "txtExp";
             this.txtExp.Size = new System.Drawing.Size(264, 23);
             this.txtExp.TabIndex = 9;
@@ -106,7 +133,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(476, 273);
+            this.label3.Location = new System.Drawing.Point(454, 310);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 16);
             this.label3.TabIndex = 8;
@@ -115,7 +142,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(476, 308);
+            this.label2.Location = new System.Drawing.Point(454, 345);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(95, 16);
             this.label2.TabIndex = 7;
@@ -124,7 +151,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(476, 240);
+            this.label1.Location = new System.Drawing.Point(454, 277);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 16);
             this.label1.TabIndex = 6;
@@ -141,23 +168,41 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Location = new System.Drawing.Point(691, 0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(539, 335);
+            this.groupBox2.Size = new System.Drawing.Size(539, 390);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "حركة الاموال";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 352);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 16);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "ج.س";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ج.س";
             // 
             // pieChart2
             // 
             this.pieChart2.Location = new System.Drawing.Point(6, 22);
             this.pieChart2.Name = "pieChart2";
-            this.pieChart2.Size = new System.Drawing.Size(512, 212);
+            this.pieChart2.Size = new System.Drawing.Size(512, 256);
             this.pieChart2.TabIndex = 13;
             this.pieChart2.Text = "pieChart2";
             // 
             // txtCust
             // 
             this.txtCust.Enabled = false;
-            this.txtCust.Location = new System.Drawing.Point(58, 294);
+            this.txtCust.Location = new System.Drawing.Point(57, 348);
             this.txtCust.Name = "txtCust";
             this.txtCust.Size = new System.Drawing.Size(264, 23);
             this.txtCust.TabIndex = 14;
@@ -165,7 +210,7 @@
             // txtSup
             // 
             this.txtSup.Enabled = false;
-            this.txtSup.Location = new System.Drawing.Point(58, 253);
+            this.txtSup.Location = new System.Drawing.Point(57, 307);
             this.txtSup.Name = "txtSup";
             this.txtSup.Size = new System.Drawing.Size(264, 23);
             this.txtSup.TabIndex = 12;
@@ -173,7 +218,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(356, 294);
+            this.label5.Location = new System.Drawing.Point(355, 348);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(152, 16);
             this.label5.TabIndex = 13;
@@ -182,62 +227,17 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(328, 253);
+            this.label4.Location = new System.Drawing.Point(327, 307);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(180, 16);
             this.label4.TabIndex = 12;
             this.label4.Text = "اجمالي استحقاقات الموردين";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(165, 243);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 16);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "ج.س";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(164, 277);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 16);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "ج.س";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(165, 311);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(38, 16);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "ج.س";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(17, 256);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(38, 16);
-            this.label9.TabIndex = 16;
-            this.label9.Text = "ج.س";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(17, 298);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(38, 16);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "ج.س";
-            // 
             // FRM_FinancialPosition
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1229, 347);
+            this.ClientSize = new System.Drawing.Size(1229, 395);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
