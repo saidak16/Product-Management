8556d3737cdacf0e83b3dee7e69a68dff80c7a1d
Ahmed Khojaly <Ahmed Khojaly@DESKTOP-A833B64>
2023-10-04 15:07:05 +0200

    Updare validations

--- Product Management/PL/FRM_OrderDetails.cs
+++ Product Management/PL/FRM_OrderDetails.cs
@@ -29,36 +29,50 @@ namespace Product_Management.PL
 
         private void txtID_TextChanged(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = orders.GetOrderDetails(txtID.Text, Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[2].Value.ToString()));
+            if (dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = orders.GetOrderDetails(txtID.Text, Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[2].Value.ToString()));
+            }
+            else
+            {
+                MessageBox.Show("لا يوجد عناصر", "");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("هل تريد استرجاع العنصر المحدد ؟؟", "عملية قيد عكسي", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            if(dataGridView1.Rows.Count > 0)
             {
-                var isValid = orders.DeleteOrderDetails(Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[0].Value.ToString()), Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[1].Value.ToString()));
-
-                if (isValid)
+                if (MessageBox.Show("هل تريد استرجاع العنصر المحدد ؟؟", "عملية قيد عكسي", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
                 {
-                    MessageBox.Show("تمت العملية بنجاح", "عملية قيد عكسي", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    
-                    if(dataGridView1.Rows.Count > 0)
+                    var isValid = orders.DeleteOrderDetails(Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[0].Value.ToString()), Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[1].Value.ToString()));
+
+                    if (isValid)
                     {
-                        dataGridView1.DataSource = orders.GetOrderDetails("", Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[2].Value.ToString()));
+                        MessageBox.Show("تمت العملية بنجاح", "عملية قيد عكسي", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        if (dataGridView1.Rows.Count > 0)
+                        {
+                            dataGridView1.DataSource = orders.GetOrderDetails("", Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[2].Value.ToString()));
+                        }
+                        else
+                        {
+                            this.Close();
+                        }
                     }
                     else
                     {
-                        this.Close();
+                        MessageBox.Show("تم إلغاء العملية", "عملية قيد عكسي", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                 }
                 else
                 {
-                    MessageBox.Show("تم إلغاء العملية", "عملية قيد عكسي", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("تم إلغاء العملية", "عملية الحذف", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else
             {
-                MessageBox.Show("تم إلغاء العملية", "عملية الحذف", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("لا يوجد عناصر", "");
             }
         }
     }
